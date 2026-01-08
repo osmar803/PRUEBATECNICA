48163d6f522cdac70222210b6fa029034e047936
osmar803 <saraviaosmar06@gmail.com>
2026-01-08 16:13:31 -0600

    creacion de infraestuctura y dbcontext

---
+++ prueba.api/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Prueba.Domain;
+
+namespace Prueba.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pais> Paises { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Municipio> Municipios { get; set; }
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Colaborador> Colaboradores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
